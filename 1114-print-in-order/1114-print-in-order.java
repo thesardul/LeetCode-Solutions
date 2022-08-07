efdcbebbe921a239552561f9d9af670c43c628c2
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-08-07 14:32:31 +0530

    Time: 25 ms (27.28%), Space: 42.9 MB (46.09%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,29 @@
+class Foo {
+
+    private Semaphore second;
+    private Semaphore third;
+
+    public Foo() {
+        second = new Semaphore(0);
+        third = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        second.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        second.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        third.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        third.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
