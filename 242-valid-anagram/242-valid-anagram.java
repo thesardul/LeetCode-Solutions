de87a5d94bdb80be62c2d58f3b3a78cac851fe1a
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-08-07 13:19:08 +0530

    Time: 4 ms (84.45%), Space: 43.8 MB (54.36%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] charsMap = new int['z'-'a'+1];
+    
+    for(char c: s.toCharArray()) {
+        int pos = c - 'a';
+        charsMap[pos]++;
+    }
+    
+    for(char c: t.toCharArray()) {
+        int pos = c - 'a';
+        charsMap[pos]--;
+    }
+    
+    for(int count: charsMap) {
+        if(count != 0) {
+            return false;
+        }
+    }
+    
+    return true;
+    }
+}
