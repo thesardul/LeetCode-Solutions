f0bdc77091f07152867157a971c575be0ce884ca
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-08-20 16:27:13 +0530

    Time: 3 ms (99.16%), Space: 48.3 MB (82.68%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,12 +1,23 @@
 class Solution {
-    public int minRefuelStops(int target, int startFuel, int[][] s) {
-        long[] dp = new long[s.length + 1];
-        dp[0] = startFuel;
-        for (int i = 0; i < s.length; ++i)
-            for (int t = i; t >= 0 && dp[t] >= s[i][0]; --t)
-                dp[t + 1] = Math.max(dp[t + 1], dp[t] + s[i][1]);
-        for (int t = 0; t <= s.length; ++t)
-            if (dp[t] >= target) return t;
-        return -1;
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int n = stations.length;
+        int maxPos = startFuel, ans = 0;
+        while (maxPos < target) {
+            int curMax = 0;
+            int curPos = -1;
+            for (int i = 0; i < n; i++) {
+                if (maxPos >= stations[i][0] && stations[i][1] > curMax) {
+                    curMax = stations[i][1];
+                    curPos = i;
+                }
+            }
+            if (curMax == 0) {
+                break;
+            }
+            ans++;
+            maxPos += curMax;
+            stations[curPos][0] = Integer.MAX_VALUE;
+        }
+        return maxPos >= target ? ans : -1;
     }
 }
