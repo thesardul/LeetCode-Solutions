ee4bca6347810ea193831d2f37fb14f35082d17a
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-09-11 08:27:52 +0530

    Time: 81 ms (51.00%), Space: 66.7 MB (82.00%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -1,17 +1,64 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        int[][] ess = new int[n][2];
-        for (int i = 0; i < n; ++i)
-            ess[i] = new int[] {efficiency[i], speed[i]};
-        Arrays.sort(ess, (a, b) -> b[0] - a[0]);
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);
-        long res = 0, sumS = 0;
-        for (int[] es : ess) {
-            pq.add(es[1]);
-            sumS = (sumS + es[1]);
-            if (pq.size() > k) sumS -= pq.poll();
-            res = Math.max(res, (sumS * es[0]));
-        }
-        return (int) (res % (long)(1e9 + 7));
+        Queue<Integer> q = new PriorityQueue<Integer>();
+		mergeSort(efficiency, speed, 0, n);
+		long sum = 0;
+		long max = 0;
+		for (int i = 0; i < speed.length; i++) {
+			if (q.size() < k) {
+				q.add(speed[i]);
+				sum += speed[i];
+				long t = sum * efficiency[i];
+				max = Long.max(max, t);
+			} else if (q.peek() < speed[i]) {
+				sum -= q.remove();
+				q.add(speed[i]);
+				sum += speed[i];
+				long t = sum * efficiency[i];
+				max = Long.max(max, t);
+			}
+		}
+		max = max % 1000000007;
+		return (int) max;
     }
+    
+    public static void mergeSort(int[] nums, int[] indices, int start, int end) {
+		if (start + 1 >= end) {
+			return;
+		}
+		int mid = (start + end) / 2;
+		mergeSort(nums, indices, start, mid);
+		mergeSort(nums, indices, mid, end);
+		merge(nums, indices, start, mid, end);
+	}
+
+	public static void merge(int[] nums, int[] indices, int start, int mid, int end) {
+		int i = start;
+		int j = mid;
+		int[] temp = new int[end - start];
+		int[] tempo = new int[end - start];
+		int k = 0;
+		while (i < mid && j < end) {
+			if (nums[i] > nums[j]) {
+				tempo[k] = indices[i];
+				temp[k++] = nums[i++];
+			} else {
+				tempo[k] = indices[j];
+				temp[k++] = nums[j++];
+			}
+		}
+		while (i < mid) {
+			tempo[k] = indices[i];
+			temp[k++] = nums[i++];
+		}
+		while (j < end) {
+			tempo[k] = indices[j];
+			temp[k++] = nums[j++];
+		}
+		k = 0;
+		for (i = start; i < end; i++) {
+			indices[i] = tempo[k];
+			nums[i] = temp[k++];
+		}
+	}
 }
