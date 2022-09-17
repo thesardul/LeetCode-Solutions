d7a5b6b9e06c4cb734a1843e26c3c5873dd7f054
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-09-17 21:11:12 +0530

    Time: 1841 ms (25.54%), Space: 214.1 MB (69.69%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.java
+++ 336-palindrome-pairs/336-palindrome-pairs.java
@@ -1,47 +1,55 @@
 class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
-    List<List<Integer>> res = new ArrayList<>(); 
-    if (words == null || words.length < 2) {
+       List<List<Integer>> res = new ArrayList<>();
+       HashMap<String,Integer> wordMap= new HashMap<>();
+       for(int i=0;i<words.length;i++){
+           wordMap.put(reverseString(words[i]),i);
+       }
+       for(int i=0;i<words.length;i++){
+           String word=words[i];
+           if(isPalindrome(word)&&wordMap.containsKey("")&&wordMap.get("")!=i){
+               res.add(Arrays.asList(i,wordMap.get("")));
+               res.add(Arrays.asList(wordMap.get(""),i));
+               
+           }
+           //String reverseWord=reverseString(word);
+           if(wordMap.containsKey(word)&&wordMap.get(word)!=i){
+               res.add(Arrays.asList(i,wordMap.get(word)));
+           }
+           //0-j,j-s.word.length()-1;
+           //s1s2
+           for(int j=1;j<word.length();j++){
+               String s1=word.substring(0,j);
+               String s2=word.substring(j,word.length());
+               // String reverseS1=reverseString(s1);
+               // String reverseS2=reverseString(s2);
+               if(wordMap.containsKey(s1)){
+                   if(isPalindrome(s2)){
+                       res.add(Arrays.asList(i,wordMap.get(s1)));
+                   }
+               }
+               if(wordMap.containsKey(s2)){
+                   if(isPalindrome(s1)){
+                       res.add(Arrays.asList(wordMap.get(s2),i));
+                   }
+               }
+           }
+               
+       }
         return res;
+        
     }
-    Map<String, Integer> map = new HashMap<String, Integer>();
-    for (int i = 0; i < words.length; i++) {
-        map.put(words[i], i);
-    }
-    for (int i = 0; i < words.length; i++) {
-        for (int j = 0; j <= words[i].length(); j++) {
-            String str1 = words[i].substring(0, j);
-            String str2 = words[i].substring(j);
-            addPair(map, res, str1, str2, i, false);
-            if(str2.length() != 0) {
-                addPair(map, res, str2, str1, i, true);
-            }
+    private boolean isPalindrome(String s){
+        if(s.length()<=1)return true;
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))return false;
+            i++;
+            j--;
         }
+        return true;
     }
-    return res;
-}
-    private void addPair(Map<String, Integer> map, List<List<Integer>> res, String str1, String str2, int index, boolean reverse) {
-    if (isPalindrome(str1)) {
-        String str2rev = new StringBuilder(str2).reverse().toString();
-        if (map.containsKey(str2rev) && map.get(str2rev) != index) {
-            List<Integer> list = new ArrayList<>();
-            if(!reverse) {
-                list.add(map.get(str2rev));
-                list.add(index);
-            } else {
-                list.add(index);
-                list.add(map.get(str2rev));
-            }
-            res.add(list);
-        }
+    private String reverseString(String s){
+        return new StringBuilder(s).reverse().toString();
     }
-}
-    private boolean isPalindrome(String str) {
-    int left = 0;
-    int right = str.length() - 1;
-    while (left <= right) {
-        if (str.charAt(left++) !=  str.charAt(right--)) return false;
-    }
-    return true;
-}
 }
