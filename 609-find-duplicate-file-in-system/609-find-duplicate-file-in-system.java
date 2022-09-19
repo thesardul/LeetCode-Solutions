0c1251d17ca9b7dc36b9520c2ac6b2cc608ec45e
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-09-19 13:29:49 +0530

    Time: 45 ms (71.55%), Space: 68.7 MB (63.48%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.java
@@ -1,28 +1,29 @@
 class Solution {
     public List<List<String>> findDuplicate(String[] paths) {
-      List<List<String>> result = new ArrayList<List<String>>();
-        int n = paths.length;
-        if (n == 0) return result;
-        
-        Map<String, Set<String>> map = new HashMap<>();
-        for (String path : paths) {
-            String[] strs = path.split("\\s+");
-            for (int i = 1; i < strs.length; i++) {
-                int idx = strs[i].indexOf("(");
-                String content = strs[i].substring(idx);
-                String filename = strs[0] + "/" + strs[i].substring(0, idx);
-                Set<String> filenames = map.getOrDefault(content, new HashSet<String>());
-                filenames.add(filename);
-                map.put(content, filenames);
+      Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        for(int i = 0; i < paths.length; i++){
+            String s = paths[i];
+            String[] str = s.split(" ");
+            String prefix = str[0] + "/";
+            for(int j = 1; j < str.length; j++){
+                String nc = str[j];
+                int t = nc.length() - 1;
+                while(t >= 0 && nc.charAt(t) != '(') t--;
+                String name = nc.substring(0, t);
+                String content = nc.substring(t + 1, nc.length() - 1);
+                map.computeIfAbsent(content, a -> new ArrayList<>());
+                map.get(content).add(prefix + name);
             }
         }
         
-        for (String key : map.keySet()) {
-            if (map.get(key).size() > 1) {
-                result.add(new ArrayList<String>(map.get(key)));
+        for(String key: map.keySet()){
+            if(map.get(key).size() > 1){
+                res.add(map.get(key));
             }
         }
         
-        return result;
+        return(res);
+        
     }
 }
