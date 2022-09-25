e7a90063e6c26d22fb012e990ebd4e9fe31b426e
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-09-25 23:06:12 +0530

    Time: 0 ms (100.00%), Space: 54.6 MB (31.06%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -5,12 +5,13 @@ class Solution {
     
     public int binarySearch(int[] a, int l, int r, int x){
         if(l <= r){
-            int mid = l + (r - l) / 2;
+            int mid = l + (r-l)/2;
             if(a[mid] == x)
                 return mid;
             if(a[mid] < x)
-                return binarySearch(a, mid + 1, r, x);
-            return binarySearch(a, l, mid - 1, x);
+                return binarySearch(a, mid+1, r, x);
+            else
+                return binarySearch(a, l, mid-1, x);
         }
         return -1;
     }
