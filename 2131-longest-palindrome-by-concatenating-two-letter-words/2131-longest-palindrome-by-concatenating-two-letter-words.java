e311fdd3a04fbeb3115d4ed948d277d73e0747c0
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-11-03 20:33:16 +0530

    Time: 25 ms (89.85%), Space: 98.4 MB (71.43%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,32 +1,21 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        HashMap<String, Integer> m = new HashMap();
-        int unpaired = 0, ans = 0;
+        int counter[][] = new int[26][26];
+        int ans = 0;
         for (String w: words) {
-            if (!m.containsKey(w)) m.put(w, 0);
-            if (w.charAt(0) == w.charAt(1)) {
-                if (m.get(w) > 0) {
-                    unpaired--;
-                    m.put(w, m.get(w) - 1);
-                    ans += 4;
-                }
-                else {
-                    m.put(w, m.get(w) + 1);
-                    unpaired++;
-                }
+            int a = w.charAt(0) - 'a', b = w.charAt(1) - 'a';
+            if (counter[b][a] > 0) {
+                ans += 4; 
+                counter[b][a]--; 
             }
-            else {
-                String rev = Character.toString(w.charAt(1)) + 
-                    Character.toString(w.charAt(0));
-                if (m.containsKey(rev) && m.get(rev) > 0) {
-                    ans += 4;
-                    m.put(rev, m.get(rev) - 1);
-                }
-                else m.put(w, m.get(w) + 1);
-            }
-
+            else counter[a][b]++;
         }
-        if (unpaired > 0) ans += 2;
+        for (int i = 0; i < 26; i++) {
+            if (counter[i][i] > 0) {
+                ans += 2;
+                break;
+            }
+        }
         return ans;
     }
 }
