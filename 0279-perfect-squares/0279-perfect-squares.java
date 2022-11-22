1132edd5204c650aa0f4a1e431f12cdbae736cae
Sardul Yadav <27910681+thesardul@users.noreply.github.com>
2022-11-22 21:47:55 +0530

    Time: 8 ms (95.88%), Space: 41.3 MB (95.44%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,17 @@
+class Solution {
+    static List<Integer> result = new ArrayList<>();
+    public int numSquares(int n) {
+        if (result.size() == 0) {
+            result.add(0);
+        }
+        while (result.size() <= n) {
+            int m = result.size();
+            int tempMin = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= m; j++) {
+                tempMin = Math.min(tempMin, result.get(m - j * j) + 1);
+            }
+            result.add(tempMin);
+        }
+        return result.get(n);
+    }
+}
